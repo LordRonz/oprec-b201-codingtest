854822534f7328b58e613a7dcd21561e3557c44b
LordRonz <aarontanhar2000@gmail.com>
2022-02-06 11:56:57 +0700

    fix: test problem 1 wrong solution heheh

--- test-problem-1/solution.cpp
+++ test-problem-1/solution.cpp
@@ -1,18 +1,16 @@
 #include <cstdio>
 
+using namespace std;
+
 int main() {
     int a[105], odd = 0, even = 0, ans = 0, n;
     scanf("%d", &n);
     for(int i = 0; i < n; ++i) {
         scanf("%d", a + i);
-        if(a[i] & 1) {
-            ++odd;
-        } else {
-            ++even;
-        }
+        a[i] & 1 ? ++odd : ++even;
     }
     for(int i = 0; i < n; ++i) {
-        if(a[i] & 1 == (odd == 1 ? 1 : 0)) {
+        if((a[i] & 1) == (odd == 1 ? 1 : 0)) {
             ans = i;
             break;
         }
